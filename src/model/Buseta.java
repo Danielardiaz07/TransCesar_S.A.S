e43546527a54be1cbbe0174889416a615432849e
Kenys Padilla <kvpadilla@unicesar.edu.co>
2026-03-12 13:54:12 -0500

    feat(model): agregar clase Buseta que hereda de Vehiculo

---
+++ src/model/Buseta.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Buseta extends Vehiculo {
+    
+    public Buseta(String placa, String ruta) {
+        super(placa, ruta);
+        this.capacidadMaxima = 19;
+        this.tarifaBase = 8000;
+    }
+
+    @Override
+    public void imprimirDetalle() {
+        System.out.printf("[BUSETA] %s | Ruta: %s | Cupos: %d | Tarifa: $%.0f | Activo: %b%n",
+            placa, ruta, getCuposDisponibles(), tarifaBase, disponible);
+    }
+
+    @Override
+    public String serializar() {
+        return "BUSETA;" + placa + ";" + ruta + ";"
+             + pasajerosActuales + ";" + disponible;
+    }    
+}
