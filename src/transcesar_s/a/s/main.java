76ffbd8495ba64ef2e8790736002fc106610cf35
Daniela Rojas Diaz <drojasd@unicesar.edu.co>
2026-03-13 16:49:12 -0500

    fix: modificar main

--- src/transcesar_s/a/s/main.java
+++ src/transcesar_s/a/s/main.java
@@ -9,23 +9,14 @@ package transcesar_s.a.s;
  * @author DANIELA ROJAS
  */
 import view.Menu;
-import service.VehiculoService;
-import service.PersonaService;
-import service.TicketService;
+
 
 public class main {
     public static void main(String[] args) {
        
-        VehiculoService vehiculoService = new VehiculoService();
-        PersonaService personaService = new PersonaService();
-        TicketService ticketService = new TicketService();
-
-        vehiculoService.cargarDatos();
-        personaService.cargarDatos();
-        ticketService.cargarDatos();
-
         Menu menu = new Menu();
         menu.mostrar();
+       
         
     }
     
