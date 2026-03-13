c2596926fd8cd27f4bb94745572b24ec535852bc
rcamilorodriguez1 <rcamilorodriguez@unicesar.edu.co>
2026-03-13 14:05:09 -0500

    feat: crear clase Ticket con atributos

---
+++ src/model/Ticket.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.time.LocalDate;
+/**
+ *
+ * @author Jose Rodriguez
+ */
+public class Ticket implements Imprimible,Calculable {
+    private Pasajero pasajero;
+    private Vehiculo vehiculo;
+    private LocalDate fechaCompra;
+    private String origen;
+    private String destino;
+    private double valorFinal;
+
+}
