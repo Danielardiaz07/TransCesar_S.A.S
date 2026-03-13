21397a2377d2af0a439cb9316156d3980d09e65f
rcamilorodriguez1 <rcamilorodriguez@unicesar.edu.co>
2026-03-13 13:25:42 -0500

    feat: crear clase abstracta Persona con atributos

---
+++ src/model/Persona.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Jose Rodriguez
+ */
+public abstract class Persona{
+    protected String cedula;
+    protected String nombre;
+}
