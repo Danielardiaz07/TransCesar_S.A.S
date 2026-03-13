e8747afeeccb3ada06981451976c3dfe26f0518d
rcamilorodriguez1 <rcamilorodriguez@unicesar.edu.co>
2026-03-13 13:35:50 -0500

    feat: agregar encapsulamiento e imprimirDetalle a Conductor

--- src/model/Conductor.java
+++ src/model/Conductor.java
@@ -11,5 +11,36 @@ package model;
 public class Conductor extends Persona{
     private String numeroLicencia;
     private String categoriaLicencia;
-    
+
+    public Conductor(String numeroLicencia, String categoriaLicencia, String cedula, String nombre) {
+        super(cedula, nombre);
+        this.numeroLicencia = numeroLicencia;
+        this.categoriaLicencia = categoriaLicencia;
+    }
+
+    public String getNumeroLicencia() {
+        return numeroLicencia;
+    }
+
+    public void setNumeroLicencia(String numeroLicencia) {
+        this.numeroLicencia = numeroLicencia;
+    }
+
+    public String getCategoriaLicencia() {
+        return categoriaLicencia;
+    }
+
+    public void setCategoriaLicencia(String categoriaLicencia) {
+        this.categoriaLicencia = categoriaLicencia;
+    }
+
+    @Override
+    public String toString() {
+        return "Conductor{" + "numeroLicencia=" + numeroLicencia + ", categoriaLicencia=" + categoriaLicencia + '}';
+    }
+
+    @Override
+    public void imprimirDetalle() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
