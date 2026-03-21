fac9d83668c6d3d7ac2469e5788175353dc9c6ba
rcamilorodriguez1 <rcamilorodriguez@unicesar.edu.co>
2026-03-20 21:29:26 -0500

    feat(model): crear clase Ruta con atributos

---
+++ src/model/Ruta.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Jose Rodriguez
+ */
+public class Ruta implements Imprimible {
+    private String codigoRuta;
+    private String ciudadOrigen;
+    private String ciudadDestino;
+    private double distanciaKm;
+    private int tiempoEstimadoMinutos;
+}
