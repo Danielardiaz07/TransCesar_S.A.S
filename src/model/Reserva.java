bff5f90f01e93e086e37c34471074e475e08f4c7
rcamilorodriguez1 <rcamilorodriguez@unicesar.edu.co>
2026-03-21 11:11:29 -0500

    feat: agregar constructor y encapsulamiento a clase Reserva

--- src/model/Reserva.java
+++ src/model/Reserva.java
@@ -17,4 +17,63 @@ public class Reserva {
     private LocalDateTime fechaCreacion;
     private LocalDate fechaViaje;
     private EstadoReserva estado;
+
+    public Reserva(String codigo, Pasajero pasajero, Vehiculo vehiculo, LocalDateTime fechaCreacion, LocalDate fechaViaje, EstadoReserva estado) {
+        this.codigo = codigo;
+        this.pasajero = pasajero;
+        this.vehiculo = vehiculo;
+        this.fechaCreacion = LocalDateTime.now();
+        this.fechaViaje = fechaViaje;
+        this.estado = EstadoReserva.ACTIVA;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Pasajero getPasajero() {
+        return pasajero;
+    }
+
+    public void setPasajero(Pasajero pasajero) {
+        this.pasajero = pasajero;
+    }
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    public LocalDateTime getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDateTime fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public LocalDate getFechaViaje() {
+        return fechaViaje;
+    }
+
+    public void setFechaViaje(LocalDate fechaViaje) {
+        this.fechaViaje = fechaViaje;
+    }
+
+    public EstadoReserva getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoReserva estado) {
+        this.estado = estado;
+    }
+    
+    
 }
