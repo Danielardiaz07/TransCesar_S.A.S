9695eb157908d993cac5d70ae437d41a459ff867
rcamilorodriguez1 <rcamilorodriguez@unicesar.edu.co>
2026-03-20 23:12:27 -0500

    feat(service): actualizar VehiculoService para asignar Ruta existente

--- src/service/VehiculoService.java
+++ src/service/VehiculoService.java
@@ -8,15 +8,20 @@ public class VehiculoService {
 
     private final VehiculoDAO dao = new VehiculoDAO();
     private final List<Vehiculo> vehiculos;
+    private final RutaService rutaService;
 
-    public VehiculoService() {
-        vehiculos = dao.cargarTodos();
+    public VehiculoService(RutaService rutaService) {
+        this.rutaService = rutaService;
+        vehiculos = dao.cargarTodos(rutaService.getRutas());
     }
 
-    public String registrarVehiculo(int tipo, String placa, String ruta) {
+    public String registrarVehiculo(int tipo, String placa, String codigoRuta) {
         for (Vehiculo v : vehiculos)
             if (v.getPlaca().equalsIgnoreCase(placa))
                 return "ERROR: Ya existe el vehículo con placa " + placa;
+         Ruta ruta = rutaService.buscarPorCodigo(codigoRuta);
+        if (ruta == null)
+            return "ERROR: No existe la ruta con código '" + codigoRuta + "'. Regístrela primero.";
 
         Vehiculo nuevo = switch (tipo) {
             case 1 -> new Buseta(placa, ruta);
