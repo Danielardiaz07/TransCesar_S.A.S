a8f04eba63d3e4b10b9ddc9ff6ce8c5723e2d092
rcamilorodriguez1 <rcamilorodriguez@unicesar.edu.co>
2026-03-20 21:32:17 -0500

    feat(model): agregar constructor y encapsulamiento a clase Ruta

--- src/model/Ruta.java
+++ src/model/Ruta.java
@@ -14,4 +14,54 @@ public class Ruta implements Imprimible {
     private String ciudadDestino;
     private double distanciaKm;
     private int tiempoEstimadoMinutos;
+
+    public Ruta(String codigoRuta, String ciudadOrigen, String ciudadDestino, double distanciaKm, int tiempoEstimadoMinutos) {
+        this.codigoRuta = codigoRuta;
+        this.ciudadOrigen = ciudadOrigen;
+        this.ciudadDestino = ciudadDestino;
+        this.distanciaKm = distanciaKm;
+        this.tiempoEstimadoMinutos = tiempoEstimadoMinutos;
+    }
+
+    public String getCodigoRuta() {
+        return codigoRuta;
+    }
+
+    public void setCodigoRuta(String codigoRuta) {
+        this.codigoRuta = codigoRuta;
+    }
+
+    public String getCiudadOrigen() {
+        return ciudadOrigen;
+    }
+
+    public void setCiudadOrigen(String ciudadOrigen) {
+        this.ciudadOrigen = ciudadOrigen;
+    }
+
+    public String getCiudadDestino() {
+        return ciudadDestino;
+    }
+
+    public void setCiudadDestino(String ciudadDestino) {
+        this.ciudadDestino = ciudadDestino;
+    }
+
+    public double getDistanciaKm() {
+        return distanciaKm;
+    }
+
+    public void setDistanciaKm(double distanciaKm) {
+        this.distanciaKm = distanciaKm;
+    }
+
+    public int getTiempoEstimadoMinutos() {
+        return tiempoEstimadoMinutos;
+    }
+
+    public void setTiempoEstimadoMinutos(int tiempoEstimadoMinutos) {
+        this.tiempoEstimadoMinutos = tiempoEstimadoMinutos;
+    }
+    
+    
 }
